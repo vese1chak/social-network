20d874fb601711f11c4686c4b12f34f75fb58436
Vlad Fadeev <vesel4ak230406vlad@mail.ru>
2024-06-13 01:29:02 +0300

    added routing and some pages

--- src/App.js
+++ src/App.js
@@ -3,19 +3,30 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Friends from "./components/Friends/Friends";
+import Music from "./components/Music/Music";
+import News from "./components/News/News";
+import Settings from "./components/Settings/Settings";
 
 function App() {
 	return (
-		<div className="app-wrapper">
-			<Header />
-			<Navbar />
-			<div className='app-wrapper-content'>
-				{/*<Profile />*/}
-				<Dialogs />
+		<BrowserRouter>
+			<div className="app-wrapper">
+				<Header />
+				<Navbar />
+				<div className='app-wrapper-content'>
+					<Routes>
+						<Route path='/profile' element={<Profile />} />
+						<Route path='/dialogs' element={<Dialogs />} />
+						<Route path='/friends' element={<Friends />} />
+						<Route path='/news' element={<News />} />
+						<Route path='/music' element={<Music />} />
+						<Route path='/settings' element={<Settings />} />
+					</Routes>
+				</div>
 			</div>
-
-
-		</div>
+		</BrowserRouter>
 	);
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,10 +5,12 @@ const Navbar = () => {
 	return (
 		<nav className={classes.nav}>
 			<ul>
-				<li><a href="#">Profile</a></li>
-				<li><a href="#">Messages</a></li>
-				<li><a href="#">News</a></li>
-				<li><a href="#">Music</a></li>
+				<li><a href="/profile">Profile</a></li>
+				<li><a href="/dialogs">Messages</a></li>
+				<li><a href="/friends">Friends</a></li>
+				<li><a href="/news">News</a></li>
+				<li><a href="/music">Music</a></li>
+				<li><a href="/settings">Settings</a></li>
 			</ul>
 		</nav>
 	);
